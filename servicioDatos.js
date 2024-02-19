3d29532aafd8ba0685447c2e81c48cebe2a056ed
JosuePenuelas <josue.penuelas@uabc.edu.mx>
2024-02-18 16:50:25 -0800

    se hizo el punto 8 de la tarea

--- servicioDatos.js
+++ servicioDatos.js
@@ -1,10 +1,11 @@
-let finDeTransmisionDatos = function (id) {
+let finDeTransmisionDatos = function (id, callback) {
     console.log('Transferencia', id, 'Terminada');
+    callback();
 }
 
-let obtenerDatosDeInternet = function (id, duracion) {
+let obtenerDatosDeInternet = function (id, duracion, callback) {
     console.log('Proceso', id, 'Obteniendo datos de Internet');
-    setTimeout(finDeTransmisionDatos, duracion, id);
+    setTimeout(finDeTransmisionDatos, duracion, id, callback);
 }
 
 module.exports.getDatos = obtenerDatosDeInternet;
